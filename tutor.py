473620eda7afffdd4bc7df93acc7288e86ec2c28
Mixalis <mstavrianakes@gmail.com>
2025-11-17 21:47:43 +0200

    Added a new agent(still has problems)

--- tutor.py
+++ tutor.py
@@ -1,12 +1,14 @@
 import os  
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+
 from langchain.agents import create_openai_tools_agent,AgentExecutor,tool
+
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate,MessagesPlaceholder
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 
 from tutor_tools import upload_to_cloud, retrieve_from_vector_db
+from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
@@ -31,9 +33,9 @@ At the end of the session:
 - Summarize the key points discussed,and using the tool ="cloud", up load to the cloud the most asked questions.
 """
 prompt = ChatPromptTemplate.from_messages([
+    ("system",TEMPLATE),
     MessagesPlaceholder(variable_name="chat_history"),
     ("human","{question}"),
-    ("system",TEMPLATE),
     MessagesPlaceholder("agent_scratchpad"),
 ])
 tools = [upload_to_cloud, retrieve_from_vector_db]
@@ -51,13 +53,47 @@ agent_executor = AgentExecutor.from_agent_and_tools(
 def generate_tutor_response(question, chat_history):
     
     print("Things are working up to here")
-    response = agent_executor.invoke(input={"question": question, "chat_history": chat_history})
-    if response is None:
-        return "I'm sorry, I couldn't generate a response at this time."
-    elif response == 0:
-        return "I'm sorry, I couldn't find relevant information in the vector database."
-    else:
-        return response['output']
+    response = agent_executor.invoke(
+        input={
+            "question": question,
+            "chat_history": chat_history,
+            }
+        )
+    final = find(question,response['output'])
+    return final
+
+
+TEMPLATE2 = "You will extract the important information from the following documents to answer the question."
+def find(question,lista):
+    llm2 = ChatGoogleGenerativeAI(
+        api_key=api_key,
+        model="gemini-2.5-flash",
+        temperature=0.2,
+    )
+    agent2 = create_openai_tools_agent(
+        llm=llm2,
+        tools=[],
+        prompt=ChatPromptTemplate.from_messages([
+            ("system",TEMPLATE2),
+            ("human","{question}"),
+            ("human","{lista}"),
+            MessagesPlaceholder("agent_scratchpad"),
+        ])
+        )
+    agent_executor2 = AgentExecutor.from_agent_and_tools(
+        agent=agent2,
+        tools=[],
+        verbose=True,
+    )
+    information = agent_executor2.invoke(
+        input={
+            "question": question,
+            "lista": lista,
+           }
+    )
+    
+    
+    return information
 
 
 def chat():
--- tutor_tools.py
+++ tutor_tools.py
@@ -9,14 +9,11 @@ def upload_to_cloud(data: str) -> str:
     print(data)
     return "Data uploaded to the cloud successfully."
 
-
-
-
 @tool("vector_db", return_direct=True)
 def retrieve_from_vector_db(query: str) -> str:
     """Retrieve relevant documents from the vector database based on the query."""
     retriever = get_retriever()
     results = retriever.get_relevant_documents(query)
     # combined_results = "\n".join([doc.page_content for doc in results])
-    print(results)
-    return 0
+    print(type(results))
+    return results
