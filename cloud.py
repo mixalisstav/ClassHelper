ab64378d5e18e383b9705385bd085e1a76c81885
Mixalis <mstavrianakes@gmail.com>
2025-11-19 12:47:58 +0200

    day(3)-> Cloud start

--- cloud.py
+++ cloud.py
@@ -7,7 +7,24 @@ frequency=5
 student_name = input("Enter student name: ")
 questions_dict = {}
 questions_dict[question] = frequency
-c.execute(f'''INSERT INTO students WHERE name={student_name} VALUES (questions)''', (questions_dict,))
 
+c.execute(
+    "INSERT INTO students (name, questions) VALUES (?, ?)",
+    (student_name, str(questions_dict))
+)
+conn.commit()
 c.close()
 conn.close()
+
+import json
+if __name__ == "__main__":
+    conn = sql.connect('classroom.db')
+    c = conn.cursor()
+    c.execute("SELECT questions FROM students WHERE name=?", (student_name,))
+    rows = c.fetchall()
+    print(rows)    
+    c.close()
+    conn.close()
+    questions_data = rows[0][0]
+    questions_dict = json.loads(questions_data.replace("'", '"'))
+    print(questions_dict)
